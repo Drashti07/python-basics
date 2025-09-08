6b316c025f1f75a5d8e9e828569f50eba632f56e
Drashti <drashtipatel72@gmail.com>
2025-09-08 09:24:46 -0400

    Add 01_String Introduxtion to string

---
+++ string_mastery/01_String.py
@@ -0,0 +1,12 @@
+'''
+String : string is nothing but sequence of character.
+        - string is immutable in python
+How to create a string in python : we can create a string by s = '<anything>'
+or s ="<Anything>"
+
+'''
+
+'''1. Creation of string : either using single quatation or double quatation '''
+
+s ='This is my first string'
+print(s)
